36f81dd379a62f48c4c7e5b69e87412eb2d5f4fc
Bukovski <kmanankov@bk.ru>
2021-01-14 12:41:19 +0300

    add: templates

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 
-const server = http.createServer(function (request, response) {
-	console.log("HTTP works!"); //при обновлении страницы выведет в консоль
+const port = 8080;
+const templateDir = __dirname + '/template/';
+const server = http.createServer();
+
+server.on('request', function(req, res) {
+	const method = req.method;
+	const _url = req.url;
+	const _baseURL = 'http://' + req.headers.host + '/';
+	const myURL = new URL(_url, _baseURL);
 	
-	response.writeHead(200, { 'Content-Type': 'text/html' });
-	response.end('<hl>Hello!</hl>'); //выведет на экран
+	if (_url === '/home' || _url === '/') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		fs.createReadStream(templateDir + 'home-table.html')
+			.pipe(res);
+		
+		// res.end('<hl>Hello!</hl>');
+	} else if (myURL.pathname === '/test' && myURL.searchParams.get('message')) {
+		//http://127.0.0.1:8080/test?message=some%20text
+		res.statusCode = 200; // OK
+		res.end(myURL.searchParams.get('message'));
+	} else if(req.url === '/api/users') {
+		const users = [{name: 'John', age: 27}, {name: 'David', age: 37}];
+		
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify(users));
+	} else {
+		res.writeHead(404, {'Content-Type': 'text/html'});
+		fs.createReadStream(templateDir + '404.html')
+			.pipe(res);
+	}
 });
 
-server.listen(8080);
+server.listen(port);
 
-console.log('http://127.0.0.1:8080');
+server.on('listening', function() {
+	console.log('http://127.0.0.1:' + port);
+});
